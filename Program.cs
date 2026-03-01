cda48cb58cdce6dfe3a16a05509c09ce35a0b112
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 21:14:42 -0600

    Logica Aplicada

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
 ﻿// Simular un menú que se repita hasta que el usuario elija salir.
+int opcion = 0;
+while (opcion != 3)
+{
+    Console.WriteLine("1) Saludar");
+    Console.WriteLine("2) Mostrar mensaje");
+    Console.WriteLine("3) Salir");
+    Console.Write("Opcion: ");
+    opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Buenos días usuario!!");
+            break;
+        case 2:
+            Console.WriteLine("Espero tengas muy lindo día!!");
+            break;
+        case 3:
+            Console.WriteLine("Saliendo del programa...");
+            break;
+        default:
+            Console.WriteLine("Opcion invalida..");
+            break;
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Programa finalizado.");
